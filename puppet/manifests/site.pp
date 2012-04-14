baf49be0e6f9f3d02ab9e200fa26cc60e45ede9b
Matthias Geisler <matthias@semanticdreamer.com>
2012-04-15 00:04:01 +0200

    testing build-couch and couchdb-debian modules

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -10,10 +10,23 @@ group { "puppet":
 /**
  * Stages
  */
-stage {"pre": before => Stage["rvm-install"]}
-class {"apt": stage => "pre"}
+# stage {"pre": before => Stage["rvm-install"]}
+stage {"pre": before => Stage["main"]}
+/**
+ * Classes
+ */
+class { "couchdb-debian":
+    version => "1.1.1-2",
+    bind_address => "0.0.0.0"
+}
+class { "apt":
+    stage => "pre",
+    always_apt_update => true
+}
 /**
  * Include modules
  */
 include apt
-include rvm
+#include rvm
+include couchdb-debian
+#include build-couchdb
