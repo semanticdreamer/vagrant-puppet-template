357c53db2c626f77a99f76265334798284afd13e
Matthias Geisler <matthias@semanticdreamer.com>
2012-05-05 00:12:34 +0200

    updated ruby 1.9.2 to p320

--- puppet/modules/ruby/manifests/init.pp
+++ puppet/modules/ruby/manifests/init.pp
@@ -3,34 +3,34 @@ class ruby {
    
    if $rvm_installed == "true" {
      rvm_system_ruby {
-         "ruby-1.9.2-p290":
+         "ruby-1.9.2-p320":
              ensure => "present",
              default_use => true;
          }
      rvm_gem {
         "puppet":
          name => "puppet",
-         ruby_version => "ruby-1.9.2-p290",
+         ruby_version => "ruby-1.9.2-p320",
          ensure => latest,
-         require => Rvm_system_ruby["ruby-1.9.2-p290"];
+         require => Rvm_system_ruby["ruby-1.9.2-p320"];
      }
      rvm_gem {
         "bundler":
          name => "bundler",
-         ruby_version => "ruby-1.9.2-p290",
+         ruby_version => "ruby-1.9.2-p320",
          ensure => latest,
-         require => Rvm_system_ruby["ruby-1.9.2-p290"];
+         require => Rvm_system_ruby["ruby-1.9.2-p320"];
      }
      #Fix for Gem repository directories are no longer world- (https://github.com/wayneeseguin/rvm/issues/398) and due to wrong umask in current rubygems version (https://github.com/rubygems/rubygems/pull/220) - not group-writable either. 
-     file { "/usr/local/rvm/gems/ruby-1.9.2-p290/cache":
+     file { "/usr/local/rvm/gems/ruby-1.9.2-p320/cache":
        mode    => 775,
        group   => "rvm"
      }
-     file { "/usr/local/rvm/gems/ruby-1.9.2-p290/doc":
+     file { "/usr/local/rvm/gems/ruby-1.9.2-p320/doc":
        mode    => 775,
        group   => "rvm"
      }
-     file { "/usr/local/rvm/gems/ruby-1.9.2-p290/gems":
+     file { "/usr/local/rvm/gems/ruby-1.9.2-p320/gems":
        mode    => 775,
        group   => "rvm"
      }
