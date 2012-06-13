1eed86e7fb282b7a3ef63305067d8a40046186a9
Matthias Geisler <matthias@semanticdreamer.com>
2012-06-13 15:53:35 +0200

    added couched custom modules to template

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -28,3 +28,19 @@ include apt
 include rvm
 include ruby
 #include nodejs
+#include build-couchdb
+# class { "build-couchdb":
+#     install_dir => '/home/vagrant',
+#     couchdb_version_tag => '1.2.0',
+#     build_user => 'vagrant',
+#     bind_address => '0.0.0.0',
+#     end_admin_party => true,
+#     admin_user => 'admin',
+#     admin_pwd => 'admin',
+#     daemon => true
+# }
+#include couchdb-debian
+#class { "couchdb-debian":
+#     version => '1.1.1-2',
+#     bind_address => '0.0.0.0'
+#}
